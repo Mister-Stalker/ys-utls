baeab92c62cce0314f58065be8a7b77136e91b45
mister_stalker <pna24052006@yandex.ru>
2026-07-10 02:07:34 +0300

    fix: UQUICConn.SetTransportParameters now stores params in quic state
    The function was missing q.conn.quic.transportParams = params,
    causing quicGetTransportParameters to block indefinitely when
    called from makeClientHelloForApplyPreset.

--- u_quic.go
+++ u_quic.go
@@ -182,15 +182,7 @@ func (q *UQUICConn) SetTransportParameters(params []byte) {
 	if params == nil {
 		params = []byte{}
 	}
-	q.conn.quic.transportParams = params // this won't be used for building ClientHello when using a preset
-
-	// // instead, we set the transport parameters hold by the ClientHello
-	// for _, ext := range q.conn.Extensions {
-	// 	if qtp, ok := ext.(*QUICTransportParametersExtension); ok {
-	// 		qtp.TransportParametersExtData = params
-	// 	}
-	// }
-
+	q.conn.quic.transportParams = params
 	if q.conn.quic.started {
 		<-q.conn.quic.signalc
 		<-q.conn.quic.blockedc
